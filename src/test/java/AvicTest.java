a17e0d884bc19f9925fca2f897ffae692a6c12a7
Cheshir-glitch <55430203+Cheshir-glitch@users.noreply.github.com>
2021-02-22 21:03:32 +0200

    init commit

--- src/test/java/AvicTest.java
+++ src/test/java/AvicTest.java
@@ -25,9 +25,9 @@ public class AvicTest {
 
     @BeforeMethod
     public void testsSetUp() {
-        driver = new ChromeDriver();//создаем экзаемпляр хром драйвера
-        driver.manage().window().maximize();//открыли браузер на весь экран
-        driver.get("https://avic.ua/");//открыли сайт
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://avic.ua/");
     }
 
     @Test(priority = 1)
@@ -44,7 +44,7 @@ public class AvicTest {
         driver.findElement(xpath("//span[text()='Телевизоры и аксессуары']")).click();
         driver.findElement(xpath("//div [@class  = 'row js_height-block category-brand-parent'] //a[text() = 'Телевизоры']")).click();
         new WebDriverWait(driver, 10).until(
-                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));//wait for page loading
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
         driver.findElement(xpath("//div [@class  = 'prod-cart__img'] /img[@class = ' lazyloaded'] [@title = 'Телевизор LG 49NANO806NA']")).click();
         new WebDriverWait(driver, 10).until(
                 webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
@@ -59,32 +59,41 @@ public class AvicTest {
     }
 
     @Test(priority = 3)
-    public void checkThatSearchResultsContainsSearchWord() {
-        driver.findElement(xpath("//input[@id='input_search']")).sendKeys("iPhone 11", ENTER);//вводим в поиск iPhone 11
-        List<WebElement> elementList = driver.findElements(xpath("//div[@class='prod-cart__descr']"));//собрали элементы поиска в лист
-        for (WebElement webElement : elementList) { //прошлись циклом и проверили что каждый элемент листа содержит текс iPhone 11
-            assertTrue(webElement.getText().contains("iPhone 11"));
-        }
+    public void checkAddToCartTwoPlayStation() {
+        driver.findElement(xpath("//input [@class= 'search-query']")).sendKeys("Sony PlayStation 5", Keys.ENTER);
+
+        driver.findElement(xpath("//div[@class = 'two-column-wrapper '] //img [@title = 'Игровая приставка Sony PlayStation 5' ]")).click();
+        new WebDriverWait(driver, 30).until(
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        driver.findElement(xpath("//a[text() = 'Купить']")).click();
+        new WebDriverWait(driver, 100).until(
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+
+        driver.findElement(xpath("//span[@class = 'js_plus btn-count btn-count--plus ']")).click();
+
+        new WebDriverWait(driver, 30).until(
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        String actualProductsCountInCart =
+                driver.findElement(xpath("//div[contains(@class,'header-bottom__cart')]//div[contains(@class,'cart_count')]")).getText();
+        assertEquals(actualProductsCountInCart, "2");
+
     }
 
     @Test(priority = 4)
     public void checkAddToCart() {
-        driver.findElement(xpath("//span[@class='sidebar-item']")).click();//каталог товаров
-        driver.findElement(xpath("//ul[contains(@class,'sidebar-list')]//a[contains(@href, 'apple-store')]")).click();//Apple Store
-        driver.findElement(xpath("//div[@class='brand-box__title']/a[contains(@href,'iphone')]")).click();//iphone
+
+        driver.findElement(xpath("//span[@class='sidebar-item']")).click();
+        driver.findElement(xpath("//span [text()='Гаджеты']")).click();
         new WebDriverWait(driver, 30).until(
-                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));//wait for page loading
-        driver.findElement(xpath("//a[@class='prod-cart__buy'][contains(@data-ecomm-cart,' Pacific Blue (MGDL3)')]")).click();//add to cart iphone
-        WebDriverWait wait = new WebDriverWait(driver, 30);//ждем пока не отобразится попап с товаром добавленным в корзину
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("js_cart")));
-        driver.findElement(xpath("//div[@class='btns-cart-holder']//a[contains(@class,'btn--orange')]")).click();//продолжить покупки
-        String actualProductsCountInCart =
-                driver.findElement(xpath("//div[contains(@class,'header-bottom__cart')]//div[contains(@class,'cart_count')]")).getText();//получили 1 которая в корзине (один продукт)
-        assertEquals(actualProductsCountInCart, "1");
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        new WebDriverWait(driver, 30).until(
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        assertTrue(driver.getCurrentUrl().contains("avic.ua/gadzhetyi1"));
+
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.close();//закрытие драйвера
+        driver.close();
     }
 }
